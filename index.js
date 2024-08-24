d455ca7834c667e6d92f2159be4b21d37335b05e
xhilgerz <alekzander.brysch@my.utsa.edu>
2024-08-23 22:49:03 -0500

    Challenge 4

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ function addGamesToPage(games) {
         <img src= ${game.img} alt="${game.name}" />
         <h3>${game.name}</h3>
         <p>${game.description}</p>
+        <p>Backers: ${game.backers}</p>
     `;
 
         // TIP: if your images are not displaying, make sure there is space
@@ -73,19 +74,39 @@ const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
 
+const totalContributions = GAMES_JSON.reduce( (acc, game) => {
+    return acc + game.backers;
+  }, 0);
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
-
+contributionsCard.innerHTML = `
+        
+        <p>$${totalContributions.toLocaleString('en-US')}</p>
+        `;
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
 
-// set inner HTML using template literal
+const totalRaised = GAMES_JSON.reduce( (acc, raised) => {
+    return acc + raised.pledged;
+  }, 0);
 
+// set inner HTML using template literal
+raisedCard.innerHTML = `
+        
+        <p>$${totalRaised.toLocaleString('en-US')}</p>
+        `;
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
 
+const totalGames = GAMES_JSON.reduce( (acc, numGames) => {
+    return acc + 1;
+  }, 0);
+
+  gamesCard.innerHTML = `
+        <p>${totalGames.toLocaleString('en-US')}</p>
+        `;
 
 /*************************************************************************************
  * Challenge 5: Add functions to filter the funded and unfunded games
